d01ad7d7d747bdc58b4688a0c7dd45715a6a0379
AlessandroSottosanti <asottosanti2@gmail.com>
2025-01-14 13:50:11 +0100

    feat: style

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
 
   const apiUrl = "https://api.themoviedb.org";
   const apiUrlFilms = "/3/search/movie?";
+  const apiUrlTvSeries = "/3/search/tv?";
+
+  const apiUrlImgs = 'https://image.tmdb.org/t/p/';
 
   useEffect(() => {
     //  getFilms();
@@ -32,30 +35,70 @@ function App() {
       });
   }
 
+  const getTvSeries = () => {
+    axios.get(`${apiUrl + apiUrlTvSeries}`,
+      {
+        params:
+        {
+          api_key: "964c51a63d6009ef4bd55d2dd351416a",
+          query: query
+        }
+      })
+      .then((resp) => {
+        console.log(resp.data.results)
+        setTvSeries(resp.data.results);
+      }).catch(() => {
+        alert("Errore nel caricamento dei film");
+      });
+  }
+
   console.log(query);
   console.log(films);
 
   return (
     <>
-      <input type="text" value={query} placeholder='Cerca...' onChange={(event) => setQuery(event.target.value)} />
-      <button onClick={() => getFilms()}>invia</button>
+
 
       <div className='container'>
+
+        <div>
+          <input type="text" value={query} placeholder='Cerca...' onChange={(event) => setQuery(event.target.value)} />
+          <button onClick={() => { getFilms(); getTvSeries(); }}>invia</button>
+        </div>
 
         {/* Lista film */}
 
         {films.map((film) =>
           <div className="card" key={film.id}>
+            {film.poster_path && <img className='img-poster' src={`${apiUrlImgs}/w342/${film.poster_path}`} alt={film.poster_path} />}
             <h1>Titolo: {film.title}</h1>
             <h2>Titolo Originale: {film.original_title}</h2>
-           {film.original_language && <span className='font-mid flag-icon-container'>lingua originale: <img className='flag-icon' src={(film.original_language === 'it' || film.original_language === 'en') ? `images/${film.original_language}.png` : `images/placeholder.png`}></img></span> }
-           <div>Numero recenzioni: {film.vote_count}</div>
-           <div className='font-mid'>Voto: <strong>{ Math.ceil(film.vote_average * 10) / 10}/10</strong></div>
+
+            <h3>Film</h3>
+            {film.original_language && <span className='font-mid flag-icon-container'>lingua originale: <img className='flag-icon' src={(film.original_language === 'it' || film.original_language === 'en') ? `images/${film.original_language}.png` : `images/placeholder.png`}></img></span>}
+            <div>Numero recenzioni: {film.vote_count}</div>
+            <div className='font-mid'>Voto: <strong>{Math.ceil(film.vote_average * 1) / 2}/5</strong></div>
           </div>
 
         )
-
         }
+
+        {/* Lista serie Tv */}
+
+        {tvSeries.map((serie) =>
+          <div className="card" key={serie.id}>
+            {serie.poster_path && <img className='img-poster' src={`${apiUrlImgs}/w342/${serie.poster_path}`} alt={serie.poster_path} />}
+            <h1>Titolo: {serie.name}</h1>
+            <h2>Titolo Originale: {serie.original_name}</h2>
+            <h3>Serie Tv</h3>
+
+            {serie.original_language && <span className='font-mid flag-icon-container'>lingua originale: <img className='flag-icon' src={(serie.original_language === 'it' || serie.original_language === 'en') ? `images/${serie.original_language}.png` : `images/placeholder.png`}></img></span>}
+            <p><strong>Descrizione:</strong>{' ' + serie.overview}</p>
+            <div>Numero recenzioni: {serie.vote_count}</div>
+            <div className='font-mid'>Voto: <strong>{Math.ceil(serie.vote_average * 10) / 10}/10</strong></div>
+          </div>
+        )}
+
       </div>
 
     </>
